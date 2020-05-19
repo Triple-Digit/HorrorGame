f1e8f236d741faa4ba6f1b1030d5755a9f661712
aaronthefknpanda <64624240+aaronthefknpanda@users.noreply.github.com>
2020-05-19 17:13:05 -0400

    Alec & Jamie's Unity Collab

---
+++ HorrorAssignment/Assets/Scripts/UItext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UItext : MonoBehaviour
+{
+    public GameObject UI_trigger;
+
+    void Start()
+    {
+       UI_trigger.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            UI_trigger.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(UI_trigger);
+        Destroy(gameObject);
+    }
+ 
+}
